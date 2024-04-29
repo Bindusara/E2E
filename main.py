f373b1909ed87c1f12a2384b53d2d6247dc8246f
Bindusara <bindusara007@gmail.com>
2024-04-29 16:01:34 +0200

    prepare_base_model

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.MRI_Classifier import logger
 from src.MRI_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.MRI_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
-STAGE_NAME = "Data Ingestion stage"
+# STAGE_NAME = "Data Ingestion stage"
 
 
 try:
@@ -13,3 +14,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
