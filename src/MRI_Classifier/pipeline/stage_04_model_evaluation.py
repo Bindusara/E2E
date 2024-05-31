3f9751cc835b4379745d80130cd8f12ab063b597
Bindusara <bindusara007@gmail.com>
2024-05-31 13:41:01 +0200

    evaulation

---
+++ src/MRI_Classifier/pipeline/stage_04_model_evaluation.py
@@ -0,0 +1,30 @@
+from src.MRI_Classifier.config.configuration import ConfigurationManager
+from src.MRI_Classifier.components.model_evaluation import Evaluation
+from src.MRI_Classifier import logger
+
+STAGE_NAME = "Evaluation stage"
+
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+            config = ConfigurationManager()
+            eval_config = config.get_evaluation_config()
+            evalluation = Evaluation(config=eval_config)
+            evalluation.evaluation()
+            evalluation.save_score()
+            evalluation.log_into_mlflow()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
